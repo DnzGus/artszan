7ca400f4d4dcee3f08e0893d9fe6eddc18488521
DnzGus <gustavodtsilva@gmail.com>
2024-09-21 21:16:01 -0300

    workin on visitor crud

---
+++ app/Http/Controllers/VisitorController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Visitor;
+use Illuminate\Support\Facades\Hash;
+
+class VisitorController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return view('visitor.profile');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('visitor.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $visitor = new Visitor();
+        $visitor->name = $request->name;
+        $visitor->email = $request->email;
+        $visitor->password = Hash::make($request->password);
+
+        $visitor->save();
+
+        return view('visitor.profile');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $visitor = Visitor::find($id);
+
+        return view('visitor.show', compact('visitor'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
